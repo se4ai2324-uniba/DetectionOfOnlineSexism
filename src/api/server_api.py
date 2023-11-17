dce03251f71b69b7acb6f00260c6485e984b2328
ELENAZAZA <m.zaza16@studenti.uniba.it>
2023-11-17 19:18:46 +0100

    Continued Fastapi

--- src/api/server_api.py
+++ src/api/server_api.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI 
-from flask import Flask, jsonify, request
 import uvicorn
 
 app = FastAPI() 
 
 @app.get("/") 
-def root(): 
-    return {
-            "Title": "MalURLs"     
-}
+def root():   
+    return corpus
 
+corpus={"Title": "Detection of Online Sexism",
+            "Description": "An NLP model used to detect sexist messages and the type of sexism.",
+            "Version": "1.0",
+            "Available endpoints": []  }
 
 if __name__ == '__main__':
    uvicorn.run(app, host='127.0.0.1', port=8000)
